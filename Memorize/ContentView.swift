84aac88eff280a02b0738bfec63bcc2dd504b456
Siddhant2155 <contactsiddhant2155@gmail.com>
2021-08-21 18:47:02 +0530

    UI Created with +/- btn for card list manipulation

--- Memorize/ContentView.swift
+++ Memorize/ContentView.swift
@@ -8,10 +8,82 @@
 import SwiftUI
 
 struct ContentView: View {
-   
+    
+    var emojis = ["✈️", "🚗", "🚌", "🚁","⛵️", "🛳", "🚄", "🚞", "🦼", "🚲", "🛺", "🚆", "🛫", "🚇", "⛴", "🏎", "🚜", "🚍"]
+    
+    @State var counter = 4
     
     var body: some View {
-      
+        VStack {
+            HStack {
+                ScrollView {
+                    LazyVGrid(columns: [GridItem(.adaptive(minimum: 75))]) {
+                        ForEach(emojis[0..<counter], id: \.self) { emoji in
+                            CardView(content: emoji)
+                                .aspectRatio(2/3, contentMode: .fit)
+                        }
+                    }
+                }
+            }
+            Spacer()
+            HStack {
+                remove
+                Spacer()
+                Text("Setting")
+                Spacer()
+                add
+            }
+            .font(.largeTitle)
+        }
+        .padding()
+    }
+    
+    var remove: some View {
+        Button {
+            if counter > 1 {
+                counter -= 1
+            }
+        } label: {
+            Image(systemName: "minus.circle")
+        }
+    }
+    
+    var add: some View {
+        Button {
+            if counter < emojis.count - 1 {
+                counter += 1
+            }
+        } label: {
+            Image(systemName: "plus.circle")
+        }
+    }
+}
+
+
+struct CardView: View {
+    @State var isFace: Bool = false
+    var content: String
+    
+    var body: some View {
+        ZStack {
+            let shape = RoundedRectangle(cornerRadius: 12)
+            if isFace {
+                shape
+                    .fill()
+                    .foregroundColor(.white)
+                shape
+                    .strokeBorder(lineWidth: 3)
+                Text(content)
+                    .font(.largeTitle)
+            } else {
+                shape
+                    .fill()
+            }
+        }
+        .foregroundColor(.red)
+        .onTapGesture {
+            isFace = !isFace
+        }
     }
 }
 
