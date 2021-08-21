34276703083123696c415db4677f02ba6220e620
Siddhant2155 <contactsiddhant2155@gmail.com>
2021-08-21 18:46:19 +0530

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Siddhant Kumar on 21/08/21.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
